4903cc04fbd2c6373087ab5196c71174d8df60f7
HamHyeonJun <hyunjun003@naver.com>
2025-02-10 23:40:53 +0900

    feat: item props전달

--- src/components/todoRemind/TodoRe.jsx
+++ src/components/todoRemind/TodoRe.jsx
@@ -1,12 +1,18 @@
 import InsertTodo from "./components/InsertTodo";
 import ListItem from "./components/ListItem";
 
+const todoItems = [
+  { id: 1, text: "투두리스트 맹글기", isDone: false },
+  { id: 2, text: "독서하기", isDone: false },
+  { id: 3, text: "공부와 취직에 대한 정신적 명상 하기", isDone: false },
+];
+
 const TodoRe = () => {
   return (
-    <div className="flex flex-col items-center">
+    <div className="container mx-auto px-4 flex flex-col items-center">
       <h1>TodoList Remind</h1>
       <InsertTodo />
-      <ListItem />
+      <ListItem items={todoItems} />
     </div>
   );
 };
--- src/components/todoRemind/components/ListItem.jsx
+++ src/components/todoRemind/components/ListItem.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const ListItem = (props) => {
-  return <div>list Components</div>;
+const ListItem = ({ items }) => {
+  return (
+    <div className="mt-8">
+      <ul>
+        {items.map((item, index) => (
+          <div className="flex">
+            <div className="w-6">{item.id}</div>
+            <li key={index} className="2-64">
+              {item.text}
+            </li>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-ListItem.propTypes = {};
+ListItem.propTypes = {
+  items: PropTypes.array.isRequired,
+};
 
 export default ListItem;
