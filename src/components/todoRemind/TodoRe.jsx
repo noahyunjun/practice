fc1884f26d54800a40da4c768654b366110c1f7c
HamHyeonJun <hyunjun003@naver.com>
2025-02-11 14:46:44 +0900

    feat: List Add items - state를 이용한 리스트 추가

--- src/components/todoRemind/TodoRe.jsx
+++ src/components/todoRemind/TodoRe.jsx
@@ -1,17 +1,28 @@
+import { useState } from "react";
 import InsertTodo from "./components/InsertTodo";
 import ListItem from "./components/ListItem";
 
-const todoItems = [
-  { id: 1, text: "투두리스트 맹글기", isDone: true },
-  { id: 2, text: "독서하기", isDone: false },
-  { id: 3, text: "공부와 취직에 대한 정신적 명상 하기", isDone: false },
-];
-
 const TodoRe = () => {
+  const [todoItems, setTodoItems] = useState([
+    { id: 1, text: "투두리스트 맹글기", isDone: true },
+    { id: 2, text: "독서하기", isDone: false },
+    { id: 3, text: "공부와 취직에 대한 정신적 명상 하기", isDone: false },
+  ]);
+
+  const addTodoList = (text) => {
+    const arr = [...todoItems];
+    arr.push({
+      id: arr.length + 1,
+      text: text,
+      isDone: false,
+    });
+    setTodoItems(arr);
+  };
+
   return (
     <div className="container mx-auto px-4 flex flex-col items-center">
       <h1>TodoList Remind</h1>
-      <InsertTodo />
+      <InsertTodo items={todoItems} addList={addTodoList} />
       <ListItem items={todoItems} />
     </div>
   );
--- src/components/todoRemind/components/InsertTodo.jsx
+++ src/components/todoRemind/components/InsertTodo.jsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Button from "./Button";
 
-const InsertTodo = (props) => {
+const InsertTodo = ({ addList }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleAddList = () => {
+    addList(inputValue);
+    setInputValue("");
+  };
   return (
     <div>
       <input
         type="text"
         placeholder="할일을 입력해주세요"
         className="border-2 mr-3"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
       />
-      <Button name={"ADD"} onClick={() => console.log("done")} />
+      <Button name={"ADD"} onClick={handleAddList} />
     </div>
   );
 };
 
-InsertTodo.propTypes = {};
+InsertTodo.propTypes = { addList: PropTypes.func.isRequired };
 
 export default InsertTodo;
