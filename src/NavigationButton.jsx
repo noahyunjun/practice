0ec150118ca319f3a3fd8431e2ba280289885f9f
HamHyeonJun <hyunjun003@naver.com>
2025-02-18 15:23:25 +0900

    fix: Link Click event - button으로 Link를 감싸놓은 부분 수정

--- src/NavigationButton.jsx
+++ src/NavigationButton.jsx
@@ -3,11 +3,12 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 const NavigationButton = ({ path, label }) => {
   return (
-    <button className="bg-blue-500 rounded m-6 px-4 py-2">
-      <Link to={path} className="text-white no-underline">
-        {label}
-      </Link>
-    </button>
+    <Link
+      to={path}
+      className="bg-blue-500 rounded m-6 px-4 py-2 text-white no-underline inline-block text-center"
+    >
+      {label}
+    </Link>
   );
 };
 
